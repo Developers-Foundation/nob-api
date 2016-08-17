fd198e3d6b63ee019446b43049581d7ad9326198
Nobody Random <iamnobodyrandom@yahoo.com>
2016-08-17 15:58:18 +0800

    init composer plugin

---
+++ src/nob-api.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 8/17/16
+ * Time: 3:29 PM
+ */
+namespace NobodyRandom\NobAPI;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class SkeletonClass
+{
+    /**
+     * Create a new Skeleton Instance
+     */
+    public function __construct()
+    {
+        // constructor body
+    }
+    /**
+     * Friendly welcome
+     *
+     * @param string $phrase Phrase to return
+     *
+     * @return string Returns the phrase passed in
+     */
+    public function echoPhrase($phrase)
+    {
+        return $phrase;
+    }
+}
